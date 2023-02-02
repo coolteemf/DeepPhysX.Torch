3ba81f865435b5e55ac957673b2129a7e4da7c15
coolteemf <francois.lecomte@inria.fr>
2023-02-02 08:18:05 +0100

    move files

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,9 @@
-__import__('pkg_resources').declare_namespace('DeepPhysX')
+from os.path import dirname
+from os import listdir
+
+package = dirname(__file__)
+exceptions = ['__init__.py', '__pycache__']
+modules = [module for module in listdir(package) if module not in exceptions]
+__all__ = []
+for module in sorted(modules):
+    __all__.append(module)
